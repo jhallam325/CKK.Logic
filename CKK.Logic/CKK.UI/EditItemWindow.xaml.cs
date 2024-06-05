b146d5c1bbdcc3aa427545a1a7fa8f1f183f304e
4400117676 <joshua.hallam7676@otech.omail.edu>
2024-06-05 13:45:41 -0600

    Working on pictures in Create new item and edit item

--- CKK.Logic/CKK.UI/EditItemWindow.xaml.cs
+++ CKK.Logic/CKK.UI/EditItemWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Windows;
 using System.Windows.Markup;
+using System.Windows.Media.Imaging;
 
 namespace CKK.UI
 {
@@ -16,6 +17,7 @@ namespace CKK.UI
     {
         private readonly IConnectionFactory connectionFactory;
         private UnitOfWork uow;
+        private bool searchButtonClicked = false;
 
         public EditItemWindow(IConnectionFactory connection, UnitOfWork uow)
         {
@@ -65,13 +67,25 @@ namespace CKK.UI
             int productID = int.Parse(productIdTextBox.Text);
             Product product = uow.Products.Get(productID);
 
+            // Validate the product
+            if (product == null)
+            {
+                MessageBox.Show("You have entered an ID number that does not exist");
+                searchButtonClicked = false;
+                return;
+            }
+
+            // We have found a product
+            searchButtonClicked = true;
+
             // Display the values of the product
             oldNameTextBox.Text = product.Name;
             oldQuantityTextBox.Text = product.Quantity.ToString();
             oldPriceTextBox.Text = product.Price.ToString();
 
             // How can I display the picture?
-            oldPictureImage = byteArrayToImage(product.Picture) as System.Windows.Controls.Image;
+            //oldPictureImage.Source = byteArrayToImage(product.Picture);// as System.Windows.Controls.Image;
+            oldPictureImage.Source = LoadImage(product.Picture);
         }
 
         public Image byteArrayToImage(byte[] bytesArr)
@@ -81,6 +95,54 @@ namespace CKK.UI
                 Image img = Image.FromStream(memstr);
                 return img;
             }
+        }
+
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Check to see if a product has been searched for
+            if (!searchButtonClicked)
+            {
+                MessageBox.Show("You must ender a product ID to be updated");
+                return;
+            }
+
+            // Find the product from the search parameter
+            int productID = int.Parse(productIdTextBox.Text);
+            Product product = uow.Products.Get(productID);
+
+            // Validate the product
+            if (product == null)
+            { 
+                MessageBox.Show("You have entered an ID number that does not exist");
+                return;
+            }
+
+            product.Name = newNameTextBox.Text;
+            product.Quantity = int.Parse(newQuantityTextBox.Text);
+            product.Price = decimal.Parse(newPriceTextBox.Text);
+            //product.Picture = 
+
+            uow.Products.Update(product);
+
+
         }
     }
 }
